0cfdffbe6350e4ebbb27b24df3199396ae78c225
DestinyMatuse <lillymatuse@gmail.com>
2024-01-19 10:44:14 -0800

    update

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,21 @@
-# Install puppet-lint
-package { 'puppet-lint':
-  ensure   => '2.5.0',
-  provider => 'gem'
+# install_python_flask_werkzeug.pp
+
+# Install python3-pip package
+package { 'python3-pip':
+  ensure => 'installed',
 }
+
+# Install Flask 2.1.0 using pip3
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install Flask==2.1.0',
+  path    => ['/usr/bin'],
+  require => Package['python3-pip'],
+}
+
+# Install Werkzeug 2.1.1 using pip3
+exec { 'install_werkzeug':
+  command => '/usr/bin/pip3 install Werkzeug==2.1.1',
+  path    => ['/usr/bin'],
+  require => Exec['install_flask'],
+}
+
