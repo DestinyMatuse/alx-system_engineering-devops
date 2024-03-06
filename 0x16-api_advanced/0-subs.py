5b2eb80485ff0cd9cbe4f8118a70d4795057dcbc
DestinyMatuse <lillymatuse@gmail.com>
2024-03-06 06:39:40 -0800

    update

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,6 +5,7 @@ subreddit.
 """
 import requests
 
+
 def number_of_subscribers(subreddit):
     """
     Queries the Reddit API and returns the number of total subscribers for a
@@ -12,23 +13,8 @@ def number_of_subscribers(subreddit):
     """
     url = 'http://www.reddit.com/r/{}/about.json'.format(subreddit)
     headers = {'User-Agent': 'Python/1.0(Holberton School 0x16 task 0)'}
-
-    try:
-        response = requests.get(url, headers=headers)
-        response.raise_for_status()  # Raise an exception for unsuccessful responses
-
-        data = response.json().get('data')
-        if data:
-            subscriber_count = data.get('subscribers')
-            return subscriber_count if subscriber_count is not None else 0
-        else:
-            return 0
-    except requests.exceptions.RequestException as e:
-        print(f"Error during request: {e}")
+    response = requests.get(url, headers=headers)
+    if (not response.ok):
         return 0
-
-# Example usage:
-subreddit_name = 'python'
-subscribers = number_of_subscribers(subreddit_name)
-print(f"The number of subscribers in r/{subreddit_name} is: {subscribers}")
-
+    subscriber_count = response.json().get('data').get('subscribers')
+    return subscriber_count
